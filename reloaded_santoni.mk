4f0b62a49515f8ab1d0466ba6ae0960965fb1d0c
iamnabilzaman <94600437+iamnabilzaman@users.noreply.github.com>
2022-01-09 01:45:13 +0600

    santoni: ReloadedOS Bringup

--- reloaded_santoni.mk
+++ reloaded_santoni.mk
@@ -27,7 +27,6 @@ $(call inherit-product, vendor/reloaded/config/common.mk)
 $(call inherit-product, device/xiaomi/santoni/device.mk)
 
 TARGET_BOOT_ANIMATION_RES := 720
-IS_PHONE := true
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 4X
